b428a61fc2455ed2b5acd3e8694e03c3f01682b3
wickdrodrck <rodrigo.ddv13@gmail.com>
2023-12-01 11:10:25 +0100

    Piton largo y duro

--- PYTHON/Ejercicios/Practica-9/Ejercicio2/Ejercicio-2.py
+++ PYTHON/Ejercicios/Practica-9/Ejercicio2/Ejercicio-2.py
@@ -2,4 +2,5 @@ import Funciones as F
 
 diccionariopersonas = F.LeerFichero()
 
-print(diccionariopersonas)
+for persona in diccionariopersonas:
+    print("Persona "+persona+"-> Nombre: "+diccionariopersonas[persona][0]+", Apellido: "+diccionariopersonas[persona][1]+", Fecha nac: "+diccionariopersonas[persona][2])
