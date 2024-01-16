6e71082a92bfb864ff28b748e1358417e20af29f
wickdrodrck <rodrigo.ddv13@gmail.com>
2024-01-16 10:29:44 +0100

    este tema maste

--- PYTHON/Django/Ejercicios/Ejercicio5/Ejercicio5/views.py
+++ PYTHON/Django/Ejercicios/Ejercicio5/Ejercicio5/views.py
@@ -6,13 +6,27 @@ def Lenguajes(request):
     if request.method == 'POST':
         nombre = request.POST['nombre_input']
         arraylengunuevos = nombre.split(",")
-        if ComprobarLenguaje(arraylengunuevos) == 1:
+        array_similares = list(filter(lambda x: x in nombre, arraylengunuevos))
+        array_diferentes = list(filter(lambda x: x not in nombre, arraylengunuevos))
+        print(len(array_similares))
+        if len(array_similares) == 1:
             mensaje = "Estás empezando..."
+            for x in array_diferentes:
+                lenguajes.append(x)
             
-        elif ComprobarLenguaje(arraylengunuevos) == 0:
+        elif len(array_similares)-1 == 0:
             mensaje = "Espabila y ponte a estudiar ya"
+            for x in array_diferentes:
+                lenguajes.append(x)
+                
         else:
-            mensaje = 
+            mensaje = array_similares
+            lenguajes.append(x)
         
     return render(request, 'index.html', {"lenguajes":lenguajes, "mensaje":mensaje})
+<<<<<<< HEAD
+
+
+=======
     
+>>>>>>> 55f12c24cfe75e651b3b92637971d745fcc6defc
